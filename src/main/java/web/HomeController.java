10857be70e831bc419650a22727d61e04a2afd59
Stephen·Chiu <qiuyj@chinacscs.com>
2018-11-05 17:33:44 +0800

    Alter class definition. Alter @RequestMapping to the class level.

--- src/main/java/web/HomeController.java
+++ src/main/java/web/HomeController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @Controller
+@RequestMapping({"/", "/home", "homepage"})
 public class HomeController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+    // @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = GET)
     public String home() {
         return "home";
     }
