558a69ba5789e64543ea109077cd663a8c7d467b
Stephen·Chiu <qiuyj@chinacscs.com>
2018-11-05 18:56:45 +0800

    add file spittles.jsp & SpittleController

---
+++ src/main/java/spittr/web/SpittleController.java
@@ -0,0 +1,28 @@
+package spittr.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import spittr.data.SpittleRespository;
+
+@Controller
+@RequestMapping({"/spittles", "/Spittles"})
+public class SpittleController {
+    private SpittleRespository spittleRespository;
+
+    @Autowired
+    public SpittleController(SpittleRespository spittleRespository) {
+        this.spittleRespository = spittleRespository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String spittles(Model model) {
+        model.addAttribute(
+                spittleRespository.findSpittles(Long.MAX_VALUE, 20)
+        );
+        return "spittles";
+    }
+}
